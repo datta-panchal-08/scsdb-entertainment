22d541b28d38e49d8c8b92b025c23e8d2852259c
Dattatray Panchal <dattapanchal464@gmail.com>
2025-03-19 20:39:26 -0700

    fix the gap between navbars

--- src/components/partials/MobileNav.jsx
+++ src/components/partials/MobileNav.jsx
@@ -3,32 +3,32 @@ import { Link } from 'react-router-dom'
 
 const MobileNav = () => {
   return (
-    <div className='w-[100%] mt-1 px-3 flex items-center gap-5 md:hidden h-[5vh]'>
+    <div className='w-[100%] mt-1 px-3 flex items-center gap-3 md:hidden h-[5vh]'>
       <h1 className="text-[1.1rem] font-bold text-white">
         <i className="text-[#6556CD] mr-2 ri-tv-fill "></i>
         <span>SCSDB.</span>
       </h1>
-         <Link to={"/trendings"} className="text-white text-[0.9rem]  font-semibold ">
-         Trending
-        </Link>
+      <Link to={"/trendings"} className="text-white text-[0.7rem]  font-semibold ">
+        Trending
+      </Link>
 
-          <Link to={"/popular"} className="text-white text-[0.9rem]  font-semibold " >
-          Popular
-        </Link>
+      <Link to={"/popular"} className="text-white text-[0.7rem]  font-semibold " >
+        Popular
+      </Link>
 
-          <Link to={"/movies"} className="text-white text-[0.9rem]  font-semibold " >
-          Movies
-        </Link>
+      <Link to={"/movies"} className="text-white text-[0.7rem]  font-semibold " >
+        Movies
+      </Link>
 
-        
-        <Link to={"/tv"} className="text-white text-[0.9rem]  font-semibold " >
-          Tv
-        </Link>
 
-        
-        <Link to={"/persons"} className="text-white text-[0.9rem] font-semibold " >
-          People
-        </Link>
+      <Link to={"/tv"} className="text-white text-[0.7rem]  font-semibold " >
+        Tv
+      </Link>
+
+
+      <Link to={"/persons"} className="text-white text-[0.7rem] font-semibold " >
+        People
+      </Link>
 
     </div>
   )
